ba2f2476bfb39be947df930aeb79879e08c2ce43
Markus Krogh <markus@casadelkrogh.dk>
2013-05-23 23:49:55 +0200

    Fixed clearInterval

--- js/views/status_view.js
+++ js/views/status_view.js
@@ -4,9 +4,13 @@ define(["util/dom","presenters/time_presenter"],function($,TimePresent){
   return {
     create: function(service){
       var view = $(".punch-clock")[0];
+      var timer;
 
       var handleCheckin = function(time_span){
-        view.textContent ="Check-in at "+TimePresent.present(time_span.start());
+        var text = "Check-in at ";
+        text += TimePresent.present(time_span.start());
+        text += " ("+time_span+")";
+        view.textContent = text;
       }
 
       var handleCheckout = function(time_span){
@@ -14,11 +18,17 @@ define(["util/dom","presenters/time_presenter"],function($,TimePresent){
         text += " - " + TimePresent.present(time_span.end());
         text += " (" + time_span + ")";
         view.textContent = text;
+
+        clearInterval(timer);
       }
 
       var track_adds = function(time_span){
-         handleCheckin(time_span);
-         time_span.trackCheckout(handleCheckout);
+        handleCheckin(time_span);
+        time_span.trackCheckout(handleCheckout);
+
+        timer = setInterval(function(){
+          handleCheckin(time_span);
+        }, 30000);
       }
 
       service.track_add(track_adds);
