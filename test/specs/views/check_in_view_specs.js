97771b9e2e8d81ff4ac3acaec39c0cb7b533ad29
Markus Krogh <markus@casadelkrogh.dk>
2013-05-27 21:43:33 +0200

    Test check-out

--- test/specs/views/check_in_view_specs.js
+++ test/specs/views/check_in_view_specs.js
@@ -14,12 +14,57 @@ define(["util/dom-creator","services/time_span_service","views/check_in_view"], 
     });
 
     describe("when clicking check-in", function(){
+      var view;
+      
+      beforeEach(function(){
+        view = CheckInView.create(service, $test);
+      });
+
       it("adds a time_span to the service", function(){
-        var view = CheckInView.create(service, $test);
         expect(service.count()).toEqual(0);
 
         $check_in.dispatchEvent(click_event);
         expect(service.count()).toEqual(1);
+      });
+
+      it("adds a time span each time", function(){
+        $check_in.dispatchEvent(click_event);
+        $check_in.dispatchEvent(click_event);
+        $check_in.dispatchEvent(click_event);
+        expect(service.count()).toEqual(3);
+        
+      });
+
+    });//END click checkin
+
+    describe("when clicking check-out after check-in", function(){
+      var view;
+      
+      beforeEach(function(){
+        view = CheckInView.create(service, $test);
+        $check_in.dispatchEvent(click_event);
+      });
+
+      it("sets the end of the checked-in time_span", function(){
+        expect(service.count()).toEqual(1);
+        expect(service.last().end()).not.toBeDefined();
+
+        $check_out.dispatchEvent(click_event);
+        expect(service.count()).toEqual(1);
+        expect(service.last().end()).toBeDefined();
+      });
+
+      it("sets the end of the last time_span added", function(){
+        $check_in.dispatchEvent(click_event);
+        expect(service.count()).toEqual(2);
+
+        expect(service.first().end()).not.toBeDefined();
+        $check_out.dispatchEvent(click_event);
+       
+        //Should be unchaged 
+        expect(service.first().end()).not.toBeDefined();
+        //Should be set
+        expect(service.last().end()).toBeDefined();
       });
 
     });//END click checkin
