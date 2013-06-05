6a2d06ce3922371305b94dbe81d9fb5d303d5324
Markus Krogh <markus@casadelkrogh.dk>
2013-06-05 18:16:28 +0200

    Added count to cleanup

--- js/views/cleanup_view.js
+++ js/views/cleanup_view.js
@@ -3,13 +3,38 @@ define(["util/dom"],function($){
   
   return {
     create: function(service, root){
-      var view = $("#cleanup", root)[0];
+      var $view = $("#cleanup", root)[0];
+      var $title = $(".title", $view)[0];
 
       var handleCleanup = function(e){
         e.preventDefault();
         service.delete_all();
       }
-      view.addEventListener("click", handleCleanup);
+
+      $view.addEventListener("click", handleCleanup);
+
+      /*
+       * Track service changes
+       */
+
+      var handleServiceChange = function(){
+        var text = $title.textContent;
+        if(text.indexOf("(") > -1){
+          text = text.replace(/\(\d+\)/, "("+service.count()+")");
+        }else{
+          text += " ("+service.count()+")";
+        }
+
+        $title.textContent = text;
+      }
+
+      service.track_add(handleServiceChange);
+
+      //On startup
+      if(service.count() > 0){
+        handleServiceChange();
+      }
+
     }
   }
 });
--- test/specs/views/cleanup_view_specs.js
+++ test/specs/views/cleanup_view_specs.js
@@ -1,20 +1,22 @@
 
 define(["util/dom-creator","services/time_span_service","views/cleanup_view"], function(DomCreator, TimeSpanService, CleanupView){
   describe("CleanupView", function(){
-    var $test, $check_in, $check_out, service, click_event;
+    var $test, $cleanup, $title, service, click_event;
+    var $view;
     beforeEach(function(){
       service = TimeSpanService.create();
       click_event = new CustomEvent("click");
 
-      $test = DomCreator(".controls");
+      $test = DomCreator(".config");
       $cleanup = DomCreator("a#cleanup");
+      $title = DomCreator("span.title");
+      $title.textContent = "clean up";
+      $cleanup.appendChild($title);
       $test.appendChild($cleanup);
     });
 
 
     describe("when clicking cleanup", function(){
-      var $view;
-      
       beforeEach(function(){
         $view = CleanupView.create(service, $test);
         service.addNew();
@@ -28,5 +30,35 @@ define(["util/dom-creator","services/time_span_service","views/cleanup_view"], f
         expect(service.count()).toEqual(0);
       });
     });//END on click
+
+    describe("when number of time spans in service change", function() {
+      beforeEach(function(){
+        $view = CleanupView.create(service, $test);
+      });
+
+      it("updates count in label when adding one", function(){
+        expect($title.textContent).toEqual("clean up");
+        service.addNew();
+        expect($title.textContent).toEqual("clean up (1)");
+      });
+
+      it("updates count in label when adding two", function(){
+        expect($title.textContent).toEqual("clean up");
+        service.addNew();
+        service.addNew();
+        expect($title.textContent).toEqual("clean up (2)");
+      });
+    });//Adding timespans
+
+    describe("on startup with saved time spans", function(){
+      beforeEach(function(){
+        service.addNew();
+        $view = CleanupView.create(service, $test);
+      });
+
+      it("label is up to date", function(){
+        expect($title.textContent).toEqual("clean up (1)");
+      });
+    });
   })//END cleanup
 });
